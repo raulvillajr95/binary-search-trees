06e84453642f46a7be52cc2adb1455b0c8653f1b
Raul <raulvillajr95@gmail.com>
2023-04-20 19:59:42 -0700

    Build tree using 'buildTree' with recursion and 'prettyPrint'

--- bst.js
+++ bst.js
@@ -1,7 +1,102 @@
 class Node {
   constructor(left, data, right) {
-    this.left = left;
+    this.left = left.length >= 1 ? buildTree(left) : null;
     this.data = data;
-    this.right = right;
+    this.right = right.length >= 1 ? buildTree(right) : null;
   }
 }
+
+class Tree {
+  constructor(arr) {
+    this.arr = arr;
+    this.root = buildTree(arr);
+  }
+}
+
+function mergeSorter(arr) {
+  // sort 2 arrays into 1
+  const sorter = (arr1, arr2, sorted = []) => {
+    if (arr1.length === 0 || arr2.length === 0) {
+      if (arr1.length === 0) {
+        sorted = [...sorted, ...arr2];
+      } else if (arr2.length === 0) {
+        sorted = [...sorted, ...arr1];
+      }
+      return sorted;
+    } else {
+      if (arr1[0] <= arr2[0]) {
+        sorted.push(arr1[0]);
+        arr1.shift();
+      }
+      if (arr1[0] > arr2[0]) {
+        sorted.push(arr2[0]);
+        arr2.shift();
+      }
+      return sorter(arr1, arr2, sorted);
+    }
+  };
+
+  const mergeSort = (arr) => {
+    if (arr.length <= 2) {
+      return arr.sort((a, b) => a - b);
+    } else {
+      return sorter(
+        mergeSort(arr.slice(0, Math.floor(arr.length / 2))),
+        mergeSort(arr.slice(Math.floor(arr.length / 2)))
+      );
+    }
+  };
+
+  return mergeSort(arr);
+}
+
+function removeDuplicatesArr(inp, counter = 0, arr = []) {
+  if (counter >= inp.length - 1) {
+    if (!arr.includes(inp[counter])) arr.push(inp[counter]);
+    return arr;
+  } else {
+    if (!arr.includes(inp[counter])) arr.push(inp[counter]);
+    return removeDuplicatesArr(inp, counter + 1, arr);
+  }
+}
+
+function buildTree(arr) {
+  let sorted = mergeSorter(arr);
+  let removedDup = removeDuplicatesArr(sorted);
+
+  let middleIndex = Math.floor((removedDup.length - 1) / 2);
+  let left = removedDup.slice(0, middleIndex);
+  let right = removedDup.slice(middleIndex + 1);
+  let middle = removedDup[middleIndex];
+
+  // return middle;
+
+  return new Node(left, middle, right);
+}
+let tree1 = buildTree([1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15]);
+let nTree = new Tree([1, 2, 3, 4, 5, 6, 7]);
+// console.log(nTree, 'Tree');
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+prettyPrint(tree1);
+
+function insert() {}
+
+function del() {
+  return 2;
+}
+
+/**
+ * build the functions inside of Tree class
+ */
